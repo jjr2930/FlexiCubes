5624d75183578004af3668102804476d0f2cee33
joojungyeol <joojungyeol@gmail.com>
2025-10-08 20:26:42 +0900

    임포트 추가.

--- examples/render.py
+++ examples/render.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
+import os
 import copy
 import math
 from ipywidgets import interactive, HBox, VBox, FloatLogSlider, IntSlider
