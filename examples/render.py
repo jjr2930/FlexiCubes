52070a17b678360a88c4fb30b095d8fe4264ff5c
jjr2930 <joojungyeol@gmail.com>
2025-10-15 23:56:41 +0900

    이동과 회전만 곱해보기

--- examples/render.py
+++ examples/render.py
@@ -35,8 +35,10 @@ def get_random_camera_batch_custom(batch_size, fovy = np.deg2rad(45), iter_res=[
             proj_mtx = util.perspective(fovy, iter_res[1] / iter_res[0], cam_near_far[0], cam_near_far[1], device=device)
             origin_position = util.translate(position[0], position[1], position[2], device=device)
             translateMatrix = util.translate(0, 0, -cam_radius, device=device)
-            rotationMatrix = util.random_rotation_translation(0.25, device=device)
-            mv     = origin_position @ translateMatrix @ rotationMatrix
+            rotationMatrix = util.random_rotation_translation(1, device=device)
+            #mv     = origin_position @ translateMatrix @ rotationMatrix
+            mv     = origin_position @ rotationMatrix
+            #mv     =  rotationMatrix
             mvp    = proj_mtx @ mv
             mv_batch.append(mv)
             mvp_batch.append(mvp)
