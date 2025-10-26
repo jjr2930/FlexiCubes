e404ff315df34c4e12a4efaa53366e55d809a9ad
jjr2930 <joojungyeol@gmail.com>
2025-10-27 01:40:02 +0900

    ...

--- examples/optimizeWithImage.py
+++ examples/optimizeWithImage.py
@@ -196,8 +196,10 @@ if __name__ == "__main__":
             mv = torch.tensor(read_mv, dtype=torch.float32, device=device)
             mvp = proj @ mv
 
-            view_img = np.load(view_path)  # [H, W, 4]
-            mask_img = np.load(mask_path)  # [H, W, 1]
+            view_full_path = os.path.join(FLAGS.working_directory, view_path)
+            mask_full_path = os.path.join(FLAGS.working_directory, mask_path)
+            view_img = np.load(view_full_path)  # [H, W, 4]
+            mask_img = np.load(mask_full_path)  # [H, W, 1]
             view = recorver_view_position(view_img, mask_img,
                                           min_x=minX, min_y=minY, min_z=minZ,
                                           max_x=maxX, max_y=maxY, max_z=maxZ)  # [H, W, 4]
