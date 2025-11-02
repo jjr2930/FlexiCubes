4de9306880d50d35264dd16e803b4066aa8a4228
joojungyeol <joojungyeol@gmail.com>
2025-11-02 20:36:47 +0900

    ..

--- examples/optimize_with_focus.py
+++ examples/optimize_with_focus.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
+from ctypes import util
+import json
 import numpy as np
 import torch
 import nvdiffrast.torch as dr
@@ -113,6 +115,19 @@ if __name__ == "__main__":
     parser.add_argument('-fcd', '--focus_capture_data', type=str, default=None)
     FLAGS = parser.parse_args()
 
+    # ==============================================================================================
+    #  parse focus data
+    # ==============================================================================================
+
+    if FLAGS.focus_capture_data is None:
+        print("No focus capture data path provided. Using default focus observe vertices.")
+        sys.exit(0)
+    
+    #load focus data it json file
+    focus_data_json = json.load(open(FLAGS.focus_capture_data, 'r'))
+    focus_data = focus_data_json["items"]
+
+
     device = 'cuda'
     
     os.makedirs(FLAGS.out_dir, exist_ok=True)
@@ -165,9 +180,27 @@ if __name__ == "__main__":
     for it in range(FLAGS.iter): 
         optimizer.zero_grad()
 
-        mv, mvp = render.get_random_camera_batch(FLAGS.batch - FLAGS.mixing_count, iter_res=FLAGS.train_res, device=device, use_kaolin=False)
-        
-        
+        if(it < FLAGS.focus_start_iteration ):
+            mv, mvp = render.get_random_camera_batch(FLAGS.batch, iter_res=FLAGS.train_res, device=device, use_kaolin=False)
+        else :
+            # sample random focus data from the loaded focus data
+            selected_focus_data = random.choices(focus_data, k=FLAGS.batch)
+
+            mv_list = []
+            mvp_list = []
+            for data in selected_focus_data:
+                position = np.array(data["cameraPosition"])
+                lookat = np.array(data["targetPosition"])
+                fovy = data["fovy"]
+                #up은 0,1,0 고정
+                up = np.array([0, 1, 0])
+                mv = util.viewMatrix(position, lookat, up, device=device)
+                projection = util.perspective(fovy, FLAGS.train_res[0]/FLAGS.train_res[1], 0.1, 1000.0, device=device)
+                mvp = projection @ mv
+                mv_list.append(mv)
+                mvp_list.append(mvp)
+            mv = torch.stack(mv_list, dim=0)
+            mvp = torch.stack(mvp_list, dim=0)
     # ==============================================================================================
     #  original code
     # ==============================================================================================   
